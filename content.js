d20bd3139c626d033015328ed0b560c22c469368
GitDaisy <100Daisy@protonmail.com>
2022-07-01 10:44:47 +0200

    Do not log things into console.
    Do not spam log in site console.

--- content.js
+++ content.js
@@ -6,10 +6,8 @@ chrome.storage.sync.get("url", function(array){
     chrome.storage.sync.get("saveLast", function(status){
       if (status.saveLast) {
         let klasa = localStorage.getItem('klasa');
-        console.log(`Last known choice is ${klasa}.`);
         for (const a of lista.contentDocument.querySelectorAll("a")) {
           if (a.textContent.includes(klasa)) {
-            console.log('Restoring choice!');
             a.click();
             break;
           }
@@ -106,20 +104,15 @@ chrome.storage.sync.get("url", function(array){
         if (themeChangePending.isChangePending) {
           chrome.storage.sync.get("onLoadAnim", function(animation){
             if (animation.onLoadAnim) {
-              console.log('Theme change pending, Starting FadeIn animation.');
               document.head.appendChild(animate);
             }
           });
           chrome.storage.sync.set({ "isChangePending": false }, function(){
-            console.log('Theme change finished!')
           });    
         }
       });
       chrome.storage.sync.get("theme", function(theme){
-        console.log(`Theme is set to ${theme.theme}.`);
-        console.log('Applying theme for side panel.');
         lista.contentDocument.head.appendChild(window[theme.theme][1]);
-        console.log('Applying theme for frame.')
       });
     ramka.onload = function() {
       chrome.storage.sync.get("theme", function(theme){
@@ -128,7 +121,6 @@ chrome.storage.sync.get("url", function(array){
       chrome.storage.sync.get("saveLast", function(status){
         if (status.saveLast) {
           var klasa = ramka.contentDocument.getElementsByClassName("tytulnapis")[0].innerHTML;
-          console.log(`New known choice ${klasa}!`);
           localStorage.setItem('klasa', klasa);
         }
       });
